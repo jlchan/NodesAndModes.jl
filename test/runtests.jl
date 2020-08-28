4e383c40b70b1592b67704dac2d3dcd9dcac6cfc
Jesse Chan <jesse.chan@rice.edu>
2020-08-27 23:45:31 -0500

    adding more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
 @testset "1D Basis" begin
     tol = 1e2*eps()
 
-    N = 4
+    N = 2
     r,w = gauss_quad(0,0,N)
     @test sum(w) ≈ 2
     @test abs(sum(w.*r)) < tol
@@ -24,4 +24,8 @@ using LinearAlgebra
 
     V = vandermonde_1D(N-1,r)
     @test V'*diagm(w)*V ≈ I
+
+    Dr = grad_vandermonde_1D(N,r)/vandermonde_1D(N,r)    
+    @test norm(sum(Dr,dims=2)) < tol
+    @test Dr*r ≈ ones(N+1)
 end
