5cb5c0f280bb4075eb376e33e046995b8704db84
Jesse Chan <jesse.chan@rice.edu>
2020-08-27 23:06:41 -0500

    Files generated by PkgTemplates

---
+++ src/NodesAndModes.jl
@@ -0,0 +1,5 @@
+module NodesAndModes
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NodesAndModes
+using Test
+
+@testset "NodesAndModes.jl" begin
+    # Write your own tests here.
+end
