e3cf0970db4fb899f501d5441c296399c7a46be6
Jesse Chan <jesse.chan@rice.edu>
2020-08-27 23:43:01 -0500

    adding tests

--- src/NodesAndModes.jl
+++ src/NodesAndModes.jl
@@ -1,5 +1,53 @@
 module NodesAndModes
 
+using LinearAlgebra
+using SpecialFunctions
+
+export greet
 greet() = print("Hello World!")
+
+include("nodes_and_modes.jl")
+
+# export 1D routines by default (there's only one type of element in 1D)
+export gauss_lobatto_quad, gauss_quad
+export jacobiP, grad_jacobiP
+
+# export 1D basis by default
+export vandermonde_1D, grad_vandermonde_1D
+
+"""
+    vandermonde_1D(N, r)
+
+Initialize the 1D Vandermonde matrix of order N Legendre polynomials at nodes r
+
+# Examples
+```jldoctest
+"""
+function vandermonde_1D(N, r)
+    V1D = zeros(length(r), N+1)
+    for j = 1:N+1
+        V1D[:,j] = jacobiP(r[:], 0, 0, j-1)
+    end
+    return V1D
+end
+
+"""
+    grad_vandermonde_1D(N, r)
+
+Initialize the 1D Vandermonde matrix of order N Legendre polynomials at nodes r
+
+# Examples
+```jldoctest
+"""
+function grad_vandermonde_1D(N, r)
+    V1D = zeros(length(r), N+1)
+    for j = 1:N+1
+        V1D[:,j] = grad_jacobiP(r[:], 0, 0, j-1)
+    end
+    return V1D
+end
+
+
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
 using NodesAndModes
 using Test
+using LinearAlgebra
 
-@testset "NodesAndModes.jl" begin
-    # Write your own tests here.
+@testset "1D Basis" begin
+    tol = 1e2*eps()
+
+    N = 4
+    r,w = gauss_quad(0,0,N)
+    @test sum(w) ≈ 2
+    @test abs(sum(w.*r)) < tol
+
+    V = vandermonde_1D(N,r)
+    @test V'*diagm(w)*V ≈ I
+
+    Vr = grad_vandermonde_1D(N,r)
+    Dr = Vr/V
+    @test norm(sum(Dr,dims=2)) < tol
+    @test Dr*r ≈ ones(N+1)
+
+    r,w = gauss_lobatto_quad(0,0,N)
+    @test sum(w) ≈ 2
+    @test abs(sum(w.*r)) < tol
+
+    V = vandermonde_1D(N-1,r)
+    @test V'*diagm(w)*V ≈ I
 end
