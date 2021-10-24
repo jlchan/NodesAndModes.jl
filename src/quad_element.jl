817abd446b3e5e19bc58ef0f43337134956782cf
Jesse Chan <jesse.chan@rice.edu>
2021-10-23 21:28:47 -0500

    reordering quad basis to induce Kronecker structure

--- src/quad_element.jl
+++ src/quad_element.jl
@@ -1,13 +1,15 @@
 
-function basis(elem::Quad,N,r,s)
-    Np = convert(Int,(N+1)*(N+1))
+function basis(elem::Quad, N, r, s)
+    Np = convert(Int, (N + 1) * (N + 1))
     sk = 1
-    V,Vr,Vs = ntuple(x->zeros(length(r), Np),3)
-    for i=0:N
-        for j=0:N
-            V[:,sk]  = jacobiP(r, 0, 0, i).*jacobiP(s, 0, 0, j)
-            Vr[:,sk] = grad_jacobiP(r, 0, 0, i).*jacobiP(s, 0, 0, j)
-            Vs[:,sk] = jacobiP(r, 0, 0, i).*grad_jacobiP(s, 0, 0, j)
+    V, Vr, Vs = ntuple(x->zeros(length(r), Np), 3)
+    for j = 0:N
+        P_j = jacobiP(s, 0, 0, j)
+        for i = 0:N
+            P_i = jacobiP(r, 0, 0, i)
+            V[:, sk]  = P_i .* P_j
+            Vr[:, sk] = grad_jacobiP(r, 0, 0, i) .* P_j
+            Vs[:, sk] = P_i .* grad_jacobiP(s, 0, 0, j)
             sk += 1
         end
     end
@@ -15,23 +17,23 @@ function basis(elem::Quad,N,r,s)
 end
 
 
-function nodes(elem::Quad,N)
-    r1D,w1D = gauss_lobatto_quad(0,0,N)
-    s,r = meshgrid(r1D)
+function nodes(elem::Quad, N)
+    r1D, w1D = gauss_lobatto_quad(0, 0, N)
+    s, r = meshgrid(r1D)
     return r[:], s[:]
 end
 
 
-function equi_nodes(elem::Quad,N)
-    r1D = LinRange(-1,1,N+1)
-    s,r = meshgrid(r1D)
+function equi_nodes(elem::Quad, N)
+    r1D = LinRange(-1, 1, N + 1)
+    s, r = meshgrid(r1D)
     return r[:], s[:]
 end
 
 function quad_nodes(elem::Quad,N)
-    r1D,w1D = gauss_quad(0,0,N)
-    s,r = meshgrid(r1D)
-    ws,wr = meshgrid(w1D)
-    w = @. wr*ws
+    r1D, w1D = gauss_quad(0, 0, N)
+    s, r = meshgrid(r1D)
+    ws, wr = meshgrid(w1D)
+    w = @. wr * ws
     return r[:], s[:], w[:]
 end
