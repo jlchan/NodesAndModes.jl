d496098e4eaa74f2049bf95b806ffb8a07cd501a
Jesse Chan <jesse.chan@rice.edu>
2022-12-14 17:00:42 -0600

    cleanup, add Quad functionality, face_basis signature
    cleanup

--- src/warpblend_interp_nodes.jl
+++ src/warpblend_interp_nodes.jl
@@ -10,18 +10,19 @@ Returns list of edges for a specific element (elem = Tri(), Pyr(), or Tet()).
 """
 get_edge_list(elem::AbstractElemShape)
 
-get_edge_list(elem::Tri) = [1,2],[2,3],[3,1]
-get_edge_list(elem::Tet) = [1,4],[4,3],[3,1],[1,2],[3,2],[4,2]
-get_edge_list(elem::Pyr) = [1,2],[2,4],[3,4],[3,1],[1,5],[2,5],[3,5],[4,5]
+get_edge_list(elem::Tri)  = [1,2], [2,3], [3,1]
+get_edge_list(elem::Quad) = [1,2], [2,3], [3,4], [4,1] # not used directly but included for completeness
+get_edge_list(elem::Tet)  = [1,4], [4,3], [3,1], [1,2], [3,2], [4,2]
+get_edge_list(elem::Pyr)  = [1,2], [2,4], [3,4], [3,1], [1,5], [2,5], [3,5], [4,5]
 
-get_vertices(elem::AbstractElemShape) = equi_nodes(elem,1)
+get_vertices(elem::AbstractElemShape) = equi_nodes(elem, 1)
 get_vertex_fxns(elem::AbstractElemShape) =
-    (rst...)->vandermonde(elem,1,rst...)/vandermonde(elem,1,equi_nodes(elem,1)...)
+    (rst...)->vandermonde(elem, 1, rst...) / vandermonde(elem, 1, equi_nodes(elem, 1)...)
 
 # assumes r1D in [-1,1], v1,v2 are vertices
-function interp_1D_to_line(r1D,v1,v2)
-    runit = @. (1+r1D)/2
-    return map((a,b)->(b-a)*runit .+ a,v1,v2)
+function interp_1D_to_line(r1D, v1, v2)
+    runit = @. (1 + r1D) / 2
+    return map((a,b) -> (b-a) * runit .+ a, v1, v2)
 end
 
 """
@@ -43,13 +44,16 @@ end
 """
     edge_basis(elem::AbstractElemShape, N, rst...)
 
-returns generalized Vandermonde matrix evaluated using an edge basis.
+Returns the generalized Vandermonde matrix evaluated using an edge basis (e.g.,
+degree `N` polynomials over an edge, but linearly blended into the interior). The 
+dimension of the resulting space is simply the number of total nodes on edges of 
+a degree `N` element. 
 """
 function edge_basis(elem::AbstractElemShape, N, rst...)
     vertices = get_vertices(elem)
     edges = get_edge_list(elem)
     vertex_functions = get_vertex_fxns(elem)
-    return edge_basis(N, vertices, edges, (N,r)->basis(Line(),N,r), vertex_functions, rst...)
+    return edge_basis(N, vertices, edges, (N, r)->basis(Line(), N, r), vertex_functions, rst...)
 end
 
 """
@@ -63,50 +67,40 @@ function edge_basis(N, vertices, edges, basis1D, vertex_functions, rst...)
         return V1
     end
 
-    V = zeros(length(first(rst)),length(edges)*(N-1) + length(first(vertices)))
-    V[:,1:size(V1,2)] .= V1 # initialize vertex functions
+    V = zeros(length(first(rst)), length(edges) * (N-1) + length(first(vertices)))
+    V[:,1:size(V1, 2)] .= V1 # initialize vertex functions
 
-    id = size(V1,2)+1
+    id = size(V1,2) + 1
     for e in edges
         # edge parametrization from paper:
         # "A Comparison of High-Order Interpolation Nodes for the Pyramid"
-        r1D_edge = V1[:,e[1]]-V1[:,e[2]]
-        V1D,_ = basis1D(N-2,r1D_edge)
-        for i = 1:size(V1D,2)
-            V[:,id] = V1D[:,i].*V1[:,e[1]].*V1[:,e[2]]
+        r1D_edge = V1[:,e[1]] - V1[:,e[2]]
+        V1D, _ = basis1D(N-2,r1D_edge)
+        for i in axes(V1D, 2)
+            V[:,id] = V1D[:,i] .* V1[:,e[1]] .* V1[:,e[2]]
             id += 1
         end
     end
     return V
 end
 
+face_basis(elem::T, N, rst...) where {T <: Union{Tri, Quad}} = edge_basis(elem, N, rst...)
+
 """
-    build_warped_nodes(elem::AbstractElemShape,N,r1D)
+    build_warped_nodes(elem::AbstractElemShape, N, r1D)
 
 Computes degree N warp-and-blend interpolation nodes for elem = Tri(), Pyr(), or
 Tet() based on the 1D node set "r1D". Returns a tuple "rst" containing arrays of
 interpolation points.
 """
-function build_warped_nodes(elem::AbstractElemShape,N,r1D)
-    r1D_equi = equi_nodes(Line(),N)
-    rst_edge_equi = interp_1D_to_edges(elem,r1D_equi)
-    V_edge = edge_basis(elem,N,rst_edge_equi...)
+function build_warped_nodes(elem::AbstractElemShape, N, r1D)
+    r1D_equi = equi_nodes(Line(), N)
+    rst_edge_equi = interp_1D_to_edges(elem, r1D_equi)
+    V_edge = edge_basis(elem, N, rst_edge_equi...)
     rst_edge = interp_1D_to_edges(elem,r1D)
 
-    c = (x->V_edge\x).(rst_edge) # should be lsq solve
+    c = (x->V_edge \ x).(rst_edge) # should be lsq solve
 
-    rst_equi = equi_nodes(elem,N)
-    return (x->edge_basis(elem,N,rst_equi...)*x).(c)
+    rst_equi = equi_nodes(elem, N)
+    return (x->edge_basis(elem, N, rst_equi...) * x).(c)
 end
-
-
-# function compute_lobatto_interp(elem::AbstractElemShape,N,r1D)
-#     r1D_equi = equi_nodes(Line(),N)
-#     rst_edge_equi = interp_1D_to_edges(elem,r1D_equi)
-#     V_edge = edge_basis(elem,N,rst_edge_equi...)
-#     rst_edge = interp_1D_to_edges(elem,r1D)
-#
-#     c = (x->V_edge\x).(rst_edge)
-#     # @show ((x->V_edge*x).(c) .- rst_edge)
-#     return c
-# end
