7a4113dc05512c3c5256e2aee5bec4642305c31b
Jesse Chan <1156048+jlchan@users.noreply.github.com>
2023-03-26 15:59:57 -0500

    Update src/tet_element.jl

--- src/tet_element.jl
+++ src/tet_element.jl
@@ -100,7 +100,7 @@ function quad_nodes_tet(N)
         t = rstw[:,3]
         w = rstw[:,4]
     elseif N < 21
-        r,s,t,w = jaskowiec_sukumar_quad_nodes(Tet(),N)
+        r, s, t, w = jaskowiec_sukumar_quad_nodes(Tet(), N)
     else
         cubN = convert(Int,ceil((N+1)/2))
         r,s,t,w = stroud_quad_nodes(Tet(),cubN)
